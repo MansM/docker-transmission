8bde28a8d0f1b31791d69388ed8a76dfac311343
Mans Matulewicz <mans.matulewicz@gmail.com>
2015-11-08 21:27:14 +0100

    cleaning up

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 mosquitto_pub -h 10.0.1.52 -t downloads/1 -m "Download finished: $TR_TORRENT_NAME"
-wget "http://localhost:9091/transmission/rpc?method=torrent-stop&id=$TR_TORRENT_ID"
-echo $?
